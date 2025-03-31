7611bcef2c420c1a064e40a3156d63f729beaed7
bbkingisking <email@example.com>
2025-03-31 09:36:46 +0100

    Updated language codes to match the ISO 693-3 standard

--- src/utils.rs
+++ src/utils.rs
@@ -2,14 +2,19 @@ use std::collections::HashMap;
 
 pub fn get_language_name(code: &str) -> Option<&'static str> {
 	let lang_map: HashMap<&str, &str> = [
-		("bg", "Български"), // Bulgarian
-		("en", "English"),   // English
-		("fa", "فارسی"),     // Persian
-		("fr", "Français"),  // French
-		("ja", "日本語"),     // Japanese
-		("lzh", "文言"),      // Literary Chinese
-		("ru", "Русский"),   // Russian
-		("zh-Hans", "简体中文"), // Simplified Chinese
+		("bul", "Български"),    // Bulgarian
+		("eng", "English"),      // English
+		("fas", "فارسی"),        // Persian
+		("fra", "Français"),     // French
+		("jpn", "日本語"),        // Japanese
+		("ojp", "上代日本語"), // Classical Japanese
+		("lzh", "文言"),          // Literary Chinese
+		("rus", "Русский"),      // Russian
+		("zho-Hans", "简体中文"), // Written Mandarin in Simplified Chinese
+		("zho-Hant", "繁體中文"), // Written Mandarin in Traditional Chinese (e.g., Taiwanese poetry)
+		("yue-Hant", "粵語"),      // Written Cantonese (Traditional)
+		("mn", "Монгол"),        // Mongolian (Default Traditional script)
+		("mn-Latn", "Mongolian (Latin)"), // Mongolian in Latin script
 	].iter().cloned().collect();
 
 	lang_map.get(code).copied()
